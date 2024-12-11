fad4919b1ee4304fbb99e71464a39d8e957aefe9
e <rajaditya6201@gmail.com>
2024-12-12 01:07:17 +0530

    call weather driver function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,11 @@
 package org.aditya
 
+import org.aditya.weatherApp.weatherDriver
 
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 suspend fun main() {
-
+        weatherDriver()
 
 }
