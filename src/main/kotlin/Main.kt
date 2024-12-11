0fbb246120ab30de05d8a04c63f39d0d29df190b
e <rajaditya6201@gmail.com>
2024-12-12 01:01:56 +0530

    remove unwanted code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
 package org.aditya
 
-import org.aditya.instagramDataFetcher.getUserData
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 suspend fun main() {
-    getUserData("adityaraj5272")
+
 
 }
